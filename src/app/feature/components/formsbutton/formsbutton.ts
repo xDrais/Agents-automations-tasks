ecb49d36cab52448db9bcaf3ffa0d52eb83b3ddd
xDrais <yassin001009@gmail.com>
2025-09-22 16:20:55 +0200

    Added forms button testing component

---
+++ src/app/feature/components/formsbutton/formsbutton.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-formsbutton',
+  imports: [],
+  templateUrl: './formsbutton.html',
+  styleUrl: './formsbutton.scss'
+})
+export class Formsbutton {
+
+}
