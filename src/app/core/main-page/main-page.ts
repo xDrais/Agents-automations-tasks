9e7ed82196751ec72f4a66e88c4002e529ab93ed
xDrais <yassin001009@gmail.com>
2025-10-30 18:46:12 +0100

    Added new main page component

---
+++ src/app/core/main-page/main-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page',
+  imports: [],
+  templateUrl: './main-page.html',
+  styleUrl: './main-page.scss'
+})
+export class MainPage {
+
+}
